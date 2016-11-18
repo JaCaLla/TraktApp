0886411b08090070b3c72fd6bb204c506c8eb149
Javier Calatrava Llavería <javi.calatrava@gmail.com>
2016-11-18 18:08:17 +0100

    Project creation

---
+++ TraktApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TraktApp
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 18/11/2016.
+//  Copyright © 2016 JAVIER CALATRAVA LLAVERIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
